04fecaf0f32ccbdb3408c964b4ddeae1b1f5813f
Francisco Garcia <belcboo@gmail.com>
2018-06-18 23:06:30 -0400

    Posting gifs. Fix the amount pending.

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,13 +11,13 @@ var app = {
   defaultButtons: function() {
     for (var i = 0; i < buttons.length; i++) {
 
-      var sp = $("<button>");
-      sp.attr("id", "item-" + counter);
-      sp.attr("class", "button");
-      sp.attr("animal", buttons[i]);
-      sp.append("" + buttons[i]);
+      var sp = $("<button>"); //Creates a new button
+      sp.attr("id", "item-" + counter); //Add id with value "item-" and the actual counter value.
+      sp.attr("class", "button"); //Adds a class called button.
+      sp.attr("animal", buttons[i]); //Adds a custom attribute with the name of the animal.
+      sp.append("" + buttons[i]); //Adds the value of the animal to display in the button.
 
-      $("#buttons").append(sp);
+      $("#buttons").append(sp); //Append the new button to the assigned div in the body.
 
       //Increase the counter to have it 'up to date'.
       counter++;
@@ -46,7 +46,7 @@ var app = {
     //Cleaning text box.
     $("#animal-input").val("");
   },
-
+//Creates a new button//Creates a new button
   showGif: function() {
 
     //Generatin the QueryURL.
@@ -57,9 +57,19 @@ var app = {
     $.ajax({
       url: queryURL,
       method: "GET"
-    }).then(function(response){ //all the information queed from Giphy is stored in the object "response".
+    }).then(function(response) { //all the information queed from Giphy is stored in the object "response".
 
+      var gifSpan = $("<span>"); //Using span to have all the new images inline by default.
+      var ratingDiv = $("<div>");//This will hold the rating.
+      var gifImg = $("<img>"); //This will hold the gif.
 
+      ratingDiv.append("Rating: ", response.data[1].rating); //pulling the rating for the image.
+      gifImg.attr("src", response.data[1].images.original.url); //pulling the url for the image.
+
+      gifSpan.append(ratingDiv);//Appending the rating to the span.
+      gifSpan.append(gifImg); //Appending the image to the span.
+
+      $("#images").append(gifSpan);//Generating the span in the body of the website.
 
     })
 
@@ -79,7 +89,7 @@ $("#addAnimal").on('click', function(event) {
 });
 
 //Clicking one of the buttons.
-$(document.body).on('click', ".button", function(){
+$(document.body).on('click', ".button", function() {
 
   //Updates the userpick variable with the value
   userpick = $(this).attr("animal");
